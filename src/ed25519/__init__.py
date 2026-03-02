d31534a8f595c5bf00277fc8f09a28f39277b7f7
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2026-03-02 10:57:32 +0100

    Remove versioneer, replace setup.py with pyproject.toml, add cibuildwheel workflow

--- src/ed25519/__init__.py
+++ src/ed25519/__init__.py
@@ -6,6 +6,4 @@ from .keys import (BadSignatureError, BadPrefixError,
  create_keypair, SigningKey, VerifyingKey,
  remove_prefix, to_ascii, from_ascii) # hush pyflakes
 
-from ._version import get_versions
-__version__ = str(get_versions()['version'])
-del get_versions
+from ._version import __version__
